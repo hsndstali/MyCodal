57455dc968c63c91add5f57cca46fd560952294f
Hossein Doostali <91480950+hsndstali@users.noreply.github.com>
2023-01-12 00:35:28 +0330

    Update BalanceSheet.py

--- BalanceSheet.py
+++ BalanceSheet.py
@@ -26,6 +26,30 @@ def main() :
   pass
   ##
   ##
+	#Functions
+	def first_type(link):
+		driver.get(link)
+		name = driver.find_element_by_id('ctl00_txbSymbol').text
+		period= driver.find_element_by_id('ctl00_lblPeriod').text
+		date = driver.find_element_by_id('ctl00_lblPeriodEndToDate').text[:4]
+		r = driver.page_source
+		soup = BeautifulSoup(r, "html.parser")
+		st_df = drivetable(soup, 0).T
+		st_df.to_excel(f'C:/Users/hosein/Desktop/balance/test/{name+period+date}.xlsx')
+
+
+	def sec_type(link):
+		driver.get(link)
+		name = driver.find_element_by_id('ctl00_txbSymbol').text
+		period= driver.find_element_by_id('ctl00_lblPeriod').text
+		date = driver.find_element_by_id('ctl00_lblPeriodEndToDate').text[:4]
+		table= driver.find_element_by_id('ctl00_cphBody_ucSFinancialPosition_grdSFinancialPosition')
+		dfbase = pd.DataFrame()
+		table =WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,"#ctl00_cphBody_ucSFinancialPosition_grdSFinancialPosition"))).get_attribute("outerHTML")
+		df=pd.read_html(str(table))[0]
+		dfbase=dfbase.append(df,ignore_index=True)
+		dfbase.to_excel(f'C:/Users/hosein/Desktop/balance/test2/{name+period+date}.xlsx')
+
   def drivetable(soup, number):
     header = soup.find_all("table")[number].find("tr")
     list_header = []
@@ -51,11 +75,9 @@ def main() :
   ##
   driver = webdriver.Chrome(path, options=chrome_options)
   ##
-  # Load the announcements for each company
-  driver.get('https://my.codal.ir/fa/statements/?company_id=&my_basket=&statement_type=146&period=12&financial_years=&company_type=0&status=1&tracing_number=&publisher_state=&title=&from_date=&to_date=&parent_or_subset=1&consolidated_or_not=&per_page=100#scroll_to_results')
-  # Save the link of each row
-  links=[]
-  for i in range(100):
+	#Links of all statements
+	links=[]
+	for i in range(100):
     reports = (
                 driver.find_element_by_id("tTable")
                 .find_element_by_class_name("grid-txt")
@@ -77,22 +99,26 @@ def main() :
             url = report.get_attribute("href")
             links.append(url)
 	##
-	# '0' at the end of links means that that address contains the balance sheet
+	# To get BalanceSheet Page
 	links = [i +'0' for  i in links]
 	##
-	file_names=[]
-	for link in links:
-    try:
-        driver.get(link)
-        name = driver.find_element_by_id('ctl00_txbSymbol').text
-        period= driver.find_element_by_id('ctl00_lblPeriod').text
-        date = driver.find_element_by_id('ctl00_lblPeriodEndToDate').text[:4]
-        r = driver.page_source
-        soup = BeautifulSoup(r, "html.parser")
-        drivetable(soup, 0).T.to_excel(f'C:/Users/hosein/Desktop/{name+period+date}.xlsx')
-        file_names.append(name+period+date)
-    except:
-        continue
+	numer_of_error=0
+
+
+	for number, link in enumerate(Balance_Links):
+			print(number)
+			try:
+					driver.get(link)
+					r = driver.page_source
+					soup = BeautifulSoup(r, "html.parser")
+					st_df = drivetable(soup, 0).T
+					if len(st_df)<13:
+							sec_type(link)
+					else:
+							first_type(link)
+			except:
+					print(numer_of_error)
+					numer_of_error+=1
 ##
 
 
